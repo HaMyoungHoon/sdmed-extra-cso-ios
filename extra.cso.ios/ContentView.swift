fe5d4911469a58d36e7bc6b8a2c2a1561cc3a227
하명훈 <basicouts@gmail.com>
2025-03-29 14:48:32 +0900

    Initial Commit

---
+++ extra.cso.ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  extra.cso.ios
+//
+//  Created by 하명훈 on 3/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ extra.cso.ios/extra_cso_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  extra_cso_iosApp.swift
+//  extra.cso.ios
+//
+//  Created by 하명훈 on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct extra_cso_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
