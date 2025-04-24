cb0c6eef11b37979eff327323e7a0aed9b4215a6
HaMyoungHoon <basicouts@gmail.com>
2025-04-24 17:58:58 +0900

    background task addin mqtt disconnect

--- extra.cso.ios/base/FAppDelegate.swift
+++ extra.cso.ios/base/FAppDelegate.swift
@@ -3,6 +3,7 @@ import UserNotifications
 import SwiftUI
 
 class FAppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    var backgroundTask: UIBackgroundTaskIdentifier = .invalid
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UNUserNotificationCenter.current().delegate = self
         return true
@@ -23,5 +24,24 @@ class FAppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDel
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.banner, .sound])
+    }
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        backgroundTask = application.beginBackgroundTask {
+            application.endBackgroundTask(self.backgroundTask)
+            self.backgroundTask = .invalid
+        }
+        FDI.mqttBackgroundService.mqttDisconnect()
+    }
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        if backgroundTask != .invalid {
+            application.endBackgroundTask(backgroundTask)
+            backgroundTask = .invalid
+        }
+        Task {
+            await FDI.mqttBackgroundService.mqttInit()
+        }
     }
 }
